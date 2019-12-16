c5b12769986530eede667c5cde2459e2cc5f4c24
Lina Aoyama <laoyama@uoregon.edu>
2019-12-16 11:16:59 -0800

    plot 5 cm and 15 cm separately

--- Soil Moisture/analysis_soil_moisture_data.R
+++ Soil Moisture/analysis_soil_moisture_data.R
@@ -9,6 +9,11 @@ soil_mois1$Time <- soil_mois1$`z6-05212`
 soil_mois1 <- soil_mois1 %>%
   gather("Port 1", "Port 2", "Port 3", "Port 4", "Port 5", "Port 6", key = "Port", value = "SM" )
 
+#Make a column for soil depth
+soil_mois1 <- soil_mois1 %>%
+  mutate(Depth = ifelse(Port %in% c("Port 1", "Port 3", "Port 5"), "5 cm", "15 cm"))
+soil_mois1$Depth <- factor(soil_mois1$Depth, levels = c("5 cm", "15 cm"))
+
 require(lubridate)
 #Specify the Time column by month, date, year, and time
 soil_mois1$Time <- mdy_hm(soil_mois1$Time)
@@ -16,7 +21,9 @@ soil_mois1$Time <- mdy_hm(soil_mois1$Time)
 #Soil moisture vs time
 ggplot(soil_mois1, aes(Time, as.numeric(SM))) +
   geom_point(aes(color = Port)) +
-  ylab("Soil Moisture (m^3/m^3)") +
+  facet_wrap(~Depth, ncol = 1)+
+  ylim(0, 0.35)+
+  ylab(bquote('Soil Moisture ('~m^3/m^3 ~")")) +
   scale_color_discrete(name = "Treatment and depth", labels = c("80% cover, 5cm", "80% cover, 15cm",
                                                                     "50% cover, 5cm", "50% cover, 15cm",
                                                                     "ambient, 5cm", "ambient, 15 cm"))
