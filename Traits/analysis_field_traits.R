6ae01ea54bf7a7728c49817b4048d8651bb275a2
Lina Aoyama <laoyama@uoregon.edu>
2019-12-16 15:54:13 -0800

    Making Poa secunda field trait plots

--- Traits/analysis_field_traits.R
+++ Traits/analysis_field_traits.R
@@ -4,36 +4,49 @@ library(ggplot2)
 #Add a column for site groupings
    #Cooler sites - EOARC, Diamond, BLM, Steens
    #Warmer sites - Gund, Fernley, Butte Valley NG, Water Canyon
-field_traits <- field_traits %>%
-  mutate(site_groups = ifelse(Site %in% c("EOARC", "Diamond", "BLM", "Steens"), "Cool",
-                              ifelse(Site %in% c("Gund", "Fernley", "ButteValleyNG", "WaterCanyon"), "Warm", ""))) %>%
-  mutate(Site = factor(Site, levels = c("EOARC", "BLM", "Diamond", "Steens",
-                            "WaterCanyon","ButteValleyNG", "Gund", "Fernley")))
 
-#Height by species and location
-ggplot(field_traits, aes(Site, Height_cm)) +
-  facet_grid(~Species) +
-  geom_point(aes(color = site_groups)) +
+field_traits1 <- field_traits %>%
+  mutate(Site = if_else(Site %in% c("BLM", "Diamond"), "Steens", as.character(Site))) %>%
+  mutate(site_groups = ifelse(Site %in% c("EOARC", "Steens", "ButteValleyNG"), "Cool",
+                              ifelse(Site %in% c("Gund", "Fernley", "WaterCanyon"), "Warm", ""))) %>%
+  mutate(Site = factor(Site, levels = c("EOARC", "Steens", "ButteValleyNG", "WaterCanyon", "Gund", "Fernley"))) %>%
+  filter(Species %in% c( "Poa secunda"))
+
+library(ggpubr)
+#Height by location
+f1 <- ggplot(field_traits1, aes(Site, log(Height))) +
+  geom_jitter(aes(color = site_groups)) +
+  geom_boxplot(alpha = 0)+
+  scale_color_manual(values= c("deepskyblue","orange")) +
+  theme(axis.text.x = element_blank(), 
+        axis.title.x = element_blank())
+
+#Stomata conductance by location
+f2 <- ggplot(field_traits1, aes(Site, Stomata_Conductance)) +
+  geom_jitter(aes(color = site_groups)) +
+  geom_boxplot(alpha = 0)+
+  scale_color_manual(values= c("deepskyblue","orange")) +
+  theme(axis.text.x = element_blank(), 
+        axis.title.x = element_blank())+
+  ylab("Stomata cond")
+
+#SLA by location
+f3<- ggplot(field_traits1, aes(Site, log(SLA))) +
+  geom_jitter(aes(color = site_groups)) +
+  geom_boxplot(alpha = 0)+
+  scale_color_manual(values= c("deepskyblue","orange")) +
+  theme(axis.text.x = element_blank(), 
+        axis.title.x = element_blank())
+
+#LDMC by location
+f4 <- ggplot(field_traits1, aes(Site, LDMC)) +
+  geom_jitter(aes(color = site_groups))+
+  geom_boxplot(alpha = 0)+
   scale_color_manual(values= c("deepskyblue","orange")) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-#Stomata conductance by species and location
-ggplot(field_traits, aes(Site, Stomata_Conductance_mmol_m2s)) +
-  facet_grid(~Species) +
-  geom_point(aes(color = site_groups)) +
-  scale_color_manual(values= c("deepskyblue","orange"))+
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-#SLA by species and location
-ggplot(field_traits, aes(Site, SLA_cm2)) +
-  facet_grid(~Species) +
-  geom_point(aes(color = site_groups)) +
-  scale_color_manual(values= c("deepskyblue","orange"))+
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-#LDMC by species and location
-ggplot(field_traits, aes(Site, LDMC)) +
-  facet_grid(~Species) +
-  geom_point(aes(color = site_groups)) +
-  scale_color_manual(values= c("deepskyblue","orange")) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggarrange(f1, f2, f3, f4,  ncol = 1, nrow = 4, 
+          labels = c("a)" ,"b)",
+                     "c)", "d)"),
+          common.legend = TRUE, legend = "bottom", 
+          font.label = list(size = 10), heights = c(2, 2, 2, 3.5))
