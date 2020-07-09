270959ec0eaa4e1e32dff140cacb9142bc4de227
Lina Aoyama <laoyama@uoregon.edu>
2020-07-09 10:41:18 -0700

    emergence of elymus elymoides

--- Demography/Germination.R
+++ Demography/Germination.R
@@ -1,14 +1,16 @@
 #Setwd 
 datpath <- "C:/Users/Lina/Dropbox/Academics/Projects/Perennial Grasses Eastern Oregon/Data"
 #Load data
-germination <- read_csv(paste(datpath, "/Demography/Germination_05132020.csv", sep=""))
+library(tidyverse)
+germination <- read_csv(paste(datpath, "/Demography/Germination_06152020.csv", sep=""))
 
 library(ggplot2)
 
 germination_sorted <- germination %>%
   mutate(Genotype = if_else(Genotype %in% c("Diamond"), "Steens", as.character(Genotype))) %>%
   mutate(Genotype = if_else(Genotype %in% c("Water Canyon"), "Winnemuca", as.character(Genotype))) %>%
-  mutate(Genotype = if_else(Genotype %in% c("Fernley"), "Reno", as.character(Genotype)))
+  mutate(Genotype = if_else(Genotype %in% c("Fernley"), "Reno", as.character(Genotype))) %>%
+  mutate(Germination = pmax(Germination_counts_May , Seedling_counts_June))
   
 
 germination_sorted$Genotype <- as.factor(germination_sorted$Genotype)
@@ -19,3 +21,9 @@ germination_sorted$Genotype <- ordered(germination_sorted$Genotype, levels = c("
 ggplot(germination_sorted, aes(x = Genotype)) +
   geom_bar() + 
   facet_wrap(vars(Species), nrow = 6)
+
+#Germination of Elymus elymoides only
+ggplot(germination_sorted %>% filter(Species == "Elymus elymoides"), aes(x = Genotype, y = Germination/10)) +
+  geom_jitter(aes(col = Plot))+
+  theme_bw() +
+  labs(y = "Germination rate", col = "Treatment")
