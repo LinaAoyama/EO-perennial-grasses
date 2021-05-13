5b0a88c03a591771482a0678dd93563e9243f3fd
Lina Aoyama <laoyama@uoregon.edu>
2021-05-13 16:27:45 -0700

    add may 2021 emergence data to the plot

--- Demography/Emergence2021.R
+++ Demography/Emergence2021.R
@@ -2,7 +2,7 @@
 datpath <- "C:/Users/Lina/Dropbox/Academics/Projects/Perennial Grasses Eastern Oregon/Data"
 #Load data
 library(tidyverse)
-emergence <- read_csv(paste(datpath, "/Demography/Emergence_04132021.csv", sep=""))
+emergence <- read_csv(paste(datpath, "/Demography/Emergence_05092021.csv", sep=""))
 library(ggplot2)
 
 se<-function(x){
@@ -23,20 +23,21 @@ ggplot(emergence, aes(x = Population, y = April_13_2021_emergence_count/300, col
 
 #Summarized seedling emergence 
 emergence_summary <- emergence %>%
-  rename(March.2021 = March_23_2021_emergence_count,
-         April.2021 = April_13_2021_emergence_count) %>%
-  gather(key = "Time", value = "Emergence", March.2021, April.2021)%>%
+  rename(March = March_23_2021_emergence_count,
+         April = April_13_2021_emergence_count, 
+         May = May_9_2021_emergence_count) %>%
+  gather(key = "Time", value = "Emergence", March, April, May)%>%
   group_by(Population, PPT_Treatment, Time) %>%
   summarise(mean_emergence = mean(Emergence/300), 
             se_emergence = se(Emergence/300)) 
-emergence_summary$Population <- ordered(emergence_summary$Population, levels = c("Roar",  "Susa", "Norc",  "Elko", "Vale", "Litt"))
-emergence_summary$Time <- ordered(emergence_summary$Time, levels = c("March.2021", "April.2021"))
+emergence_summary$Population <- ordered(emergence_summary$Population, levels = c("Norc",  "Roar",  "Vale", "Susa","Elko" ,"Litt"))
+emergence_summary$Time <- ordered(emergence_summary$Time, levels = c("March", "April", "May"))
+
 
 ggplot(emergence_summary, aes(y = mean_emergence, x = Time, col = PPT_Treatment)) +
   geom_point() +
   geom_errorbar(aes(ymin = mean_emergence-se_emergence, ymax = mean_emergence+se_emergence), width = 0.4, alpha = 0.9, size = 1) +
   theme_bw() +
   facet_grid(~Population)+
-  labs(y = "Seedling emergence rate")
-
+  labs(y = "Seedling emergence rate") 
 
