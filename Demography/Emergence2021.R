c7c9aec23c1e1719dae15326bd135fe8d1a7aea2
Lina Aoyama <laoyama@uoregon.edu>
2021-06-21 15:25:38 -0700

    add june data

--- Demography/Emergence2021.R
+++ Demography/Emergence2021.R
@@ -2,7 +2,7 @@
 datpath <- "C:/Users/Lina/Dropbox/Academics/Projects/Perennial Grasses Eastern Oregon/Data"
 #Load data
 library(tidyverse)
-emergence <- read_csv(paste(datpath, "/Demography/Emergence_05092021.csv", sep=""))
+emergence <- read_csv(paste(datpath, "/Demography/Emergence_06182021.csv", sep=""))
 library(ggplot2)
 
 se<-function(x){
@@ -25,13 +25,14 @@ ggplot(emergence, aes(x = Population, y = April_13_2021_emergence_count/300, col
 emergence_summary <- emergence %>%
   rename(March = March_23_2021_emergence_count,
          April = April_13_2021_emergence_count, 
-         May = May_9_2021_emergence_count) %>%
-  gather(key = "Time", value = "Emergence", March, April, May)%>%
+         May = May_9_2021_emergence_count,
+         June = June_18_2021_emergence_count) %>%
+  gather(key = "Time", value = "Emergence", March, April, May, June)%>%
   group_by(Population, PPT_Treatment, Time) %>%
   summarise(mean_emergence = mean(Emergence/300), 
             se_emergence = se(Emergence/300)) 
 emergence_summary$Population <- ordered(emergence_summary$Population, levels = c("Norc",  "Roar",  "Vale", "Susa","Elko" ,"Litt"))
-emergence_summary$Time <- ordered(emergence_summary$Time, levels = c("March", "April", "May"))
+emergence_summary$Time <- ordered(emergence_summary$Time, levels = c("March", "April", "May", "June"))
 
 
 ggplot(emergence_summary, aes(y = mean_emergence, x = Time, col = PPT_Treatment)) +
